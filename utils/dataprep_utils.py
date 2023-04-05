f21fdc1b94c4e792731fd189eada965edba9a187
JoshuaB <65095331+JoshuaBonacorsi@users.noreply.github.com>
2023-04-05 12:21:19 +0200

    FInalCommit

--- utils/dataprep_utils.py
+++ utils/dataprep_utils.py
@@ -1,6 +1,54 @@
 import torch
 import pandas as pd
 import numpy as np
+import random
+import time
+import scipy
+
+def neg_sampling(ratings_df, n_neg=1, neg_val=0, pos_val=1, percent_print=5):
+  """version 1.2: 1 positive 1 neg (2 times bigger than the original dataset by default)
+    Parameters:
+    input rating data as pandas dataframe: userId|movieId|rating
+    n_neg: take n_negative / 1 positive
+    Returns:
+    negative sampled set as pandas dataframe
+            userId|movieId|interact (implicit)
+  """
+
+  ratings_df.userId = ratings_df.userId.astype('category').cat.codes.values
+  ratings_df.movieId = ratings_df.movieId.astype('category').cat.codes.values
+  
+  sparse_mat = scipy.sparse.coo_matrix((ratings_df.rating, (ratings_df.userId, ratings_df.movieId)))
+  dense_mat = np.asarray(sparse_mat.todense())
+  print(dense_mat.shape)
+  nsamples = ratings_df[['userId', 'movieId']]
+  nsamples['interact'] = nsamples.apply(lambda row: 1, axis=1)
+  length = dense_mat.shape[0]
+  printpc = int(length * percent_print/100)
+  nTempData = []
+  i = 0
+  start_time = time.time()
+  stop_time = time.time()
+  extra_samples = 0
+  for row in dense_mat:
+    if(i%printpc==0):
+      stop_time = time.time()
+      print("processed ... {0:0.2f}% ...{1:0.2f}secs".format(float(i)*100 / length, stop_time - start_time))
+      start_time = stop_time
+    n_non_0 = len(np.nonzero(row)[0])
+    zero_indices = np.where(row==0)[0]
+    if(n_non_0 * n_neg + extra_samples > len(zero_indices)):
+      print(i, "non 0:", n_non_0,": len ",len(zero_indices))
+      neg_indices = zero_indices.tolist()
+      extra_samples = n_non_0 * n_neg + extra_samples - len(zero_indices)
+    else:
+      neg_indices = random.sample(zero_indices.tolist(), n_non_0 * n_neg + extra_samples)
+      extra_samples = 0
+    nTempData.extend([(uu, ii, rr) for (uu, ii, rr) in zip(np.repeat(i, len(neg_indices))
+                    , neg_indices, np.repeat(neg_val, len(neg_indices)))])
+    i+=1
+  nsamples=nsamples.append(pd.DataFrame(nTempData, columns=["userId","movieId", "interact"]),ignore_index=True)
+  return nsamples
 
 class MovieDataset:
     def __init__(self, users, movies, ratings):
--- utils/models_utils.py
+++ utils/models_utils.py
@@ -1,14 +1,71 @@
 import torch
 import torch.nn as nn
 
-class DLCRS(nn.Module):
+class ExplicitDLCRS(nn.Module):
 
     def __init__(self,
                  n_users, # liste des users id
                  n_movies, # liste des movies id
                  n_latents, # nombre de latents factors 
-                 n_layers,
-                 dropout=None): # dictionnaire de configuration pour les layers fully connected
+                 n_layers, # nombre de couches dans la partie fully connected
+                 dropout=None): # taux de dropout
+
+        super().__init__()
+        
+        # Embedding part
+        self.user_embed = nn.Embedding(num_embeddings=n_users, embedding_dim=n_latents)
+        self.movie_embed = nn.Embedding(num_embeddings=n_movies, embedding_dim=n_latents)
+
+        # Fully connected layers part
+
+        self.fc_activation = nn.ReLU()
+        self.fc_layers = torch.nn.ModuleList()
+
+        if dropout :
+            # Add dropout layers 
+            self.do_layers = nn.ModuleList()
+
+        in_size, out_size = 2*n_latents,50*n_layers
+
+        for i in range(n_layers) :
+            self.fc_layers.append(torch.nn.Linear(in_size, out_size))
+            self.do_layers.append(torch.nn.Dropout(dropout))
+            in_size = out_size
+            out_size -= 50 
+
+        # Output layer part
+        self.fc_layers.append(nn.Linear(50,1))
+        
+    def forward(self, users, movies, ratings=None):
+
+        users_embed = self.user_embed(users)
+        movies_embed = self.movie_embed(movies)
+
+        # concat both vectors
+        output = torch.cat([users_embed,movies_embed],dim=1)
+
+        # Fully connected layers loop
+        for fc_layer, dropout_layer in zip(self.fc_layers[:-1], self.do_layers):
+            output = fc_layer(output)
+            output = dropout_layer(output)
+            output = self.fc_activation(output)
+
+        # Output layer
+        output = self.fc_layers[-1](output)
+        output = self.fc_activation(output)
+
+        return(output)
+
+###############################################################################################################
+
+class ImplicitDLCRS(nn.Module):
+
+    def __init__(self,
+                 n_users, # liste des users id
+                 n_movies, # liste des movies id
+                 n_latents, # nombre de latents factors 
+                 n_layers, # nombre de couches dans la partie fully connected
+                 dropout=None): # taux de dropout
 
         super().__init__()
         
@@ -52,5 +109,5 @@ class DLCRS(nn.Module):
 
         # Output layer
         output = self.fc_layers[-1](output)
-
+    
         return(output)
